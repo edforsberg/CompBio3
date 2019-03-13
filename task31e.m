6cb6283b97c8221cc183ddcb0a9b4590bdee41ba
HakonV <hakon93@gmail.com>
2019-03-13 23:53:22 +0100

    31.e

--- task31e.m
+++ task31e.m
@@ -18,17 +18,31 @@ for i = 1:nrRuns
     end 
 end
 
-distr = distr/sum(distr); 
-
-xVec = 1:numel(distr); 
+xVec = 0:numel(distr)-1; 
 gausDistr = normpdf(xVec,qss,8.9);
-plot(xVec,distr); 
+quasiDistr = quasiDist(xVec,alpha,beta,N);
+quasiDistr = quasiDistr/sum(quasiDistr)
+distr = distr/sum(distr); 
+plot(xVec(2:end),distr(2:end)); 
 hold on 
 plot(xVec,gausDistr); 
 hold on
+plot(xVec,quasiDistr);
 plot([qss qss],[0 1.2*max(distr)]); 
-legend('simulation','gausian', 'quasi steady state'); 
-title('Normalized position distribution over 1E5 runs');
+legend('Simulation','Gaussian','Theoretical','Quasi steady state'); 
+title('Normalized position probability distribution over 1E5 runs');
 xlabel('I');
 ylabel('Probability(I)'); 
+
+function I_S = quasiDist(I,alpha,beta,N)
+    I = I./N;
+    r0= alpha/beta;
+    tmp = 1-1/r0;
+    I_S = zeros(1,length(I));
+    fun = @(X)log(r0*(1-X));
+    for i = 1:length(I)
+        I_S(i) = -integral(fun,tmp,I(i));
+    end
+    I_S = exp(-I_S.*N)
+end
 
